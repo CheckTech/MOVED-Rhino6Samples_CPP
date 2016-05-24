4f6cd560597df3e30bf96944770585a1d377e8e8
Dale Fugier <dale@mcneel.com>
2016-05-24 09:05:26 -0700

    Updated SampleSkin

--- SampleSkinHandler/SampleSkinHandlerMenu.cpp
+++ SampleSkinHandler/SampleSkinHandlerMenu.cpp
@@ -17,19 +17,19 @@ BOOL CSampleSkinHandlerPlugIn::OnPlugInMenuCommand(WPARAM wParam)
   switch (wParam)
   {
   case ID_FILE_NEW:
-    ::RhinoApp().RunMenuScript(L"! _New");
+    RhinoApp().RunMenuScript(L"! _New");
     break;
   case ID_FILE_OPEN:
-    ::RhinoApp().RunMenuScript(L"! _Open");
+    RhinoApp().RunMenuScript(L"! _Open");
     break;
   case ID_FILE_SAVE:
-    ::RhinoApp().RunMenuScript(L"! _Save");
+    RhinoApp().RunMenuScript(L"! _Save");
     break;
   case ID_FILE_SAVEAS:
-    ::RhinoApp().RunMenuScript(L"! _SaveAs");
+    RhinoApp().RunMenuScript(L"! _SaveAs");
     break;
   case ID_FILE_EXIT:
-    ::RhinoApp().RunMenuScript(L"! _Exit");
+    RhinoApp().RunMenuScript(L"! _Exit");
     break;
   }
 
