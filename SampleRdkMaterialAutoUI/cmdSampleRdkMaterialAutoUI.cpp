099a114b91077ca1f58fac482ec70b491674ed26
John Croudy <john.croudy@mcneel.com>
2017-01-03 17:12:30 -0800

    Fixes compiler error due to RDK SDK change.

--- SampleRdkMaterialAutoUI/cmdSampleRdkMaterialAutoUI.cpp
+++ SampleRdkMaterialAutoUI/cmdSampleRdkMaterialAutoUI.cpp
@@ -28,7 +28,7 @@ CRhinoCommand::result CCommandSampleRdkMaterialAutoUI::RunCommand( const CRhinoC
 	auto pContent = new CSampleRdkMaterial;
 	pContent->Initialize();
 
-	rdkDoc.AttachContent(*pContent);
+	rdkDoc.TakeOwnershipOfContent(*pContent);
 
 	rdkDoc.EndChange();
 
